f02ea8f800d1d35be08573fb84c7378adf79d951
junzhong loo <junzhongloo@gmail.com>
2020-05-04 22:30:17 +0800

    start show channels END PC

--- application.py
+++ application.py
@@ -7,8 +7,24 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+class Channel:
+    def __init__(self, name, messages = []):
+        self.name = name
+        self.messages = messages
+
+cs50x = Channel(name= "cs50x")
+cs50w = Channel(name= "cs50w")
+
+channels = []
+channels.append(cs50x)
+channels.append(cs50w)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/channels")
+def channels():
+    channels = channels
+    return jsonify({"success": True, "channels": channels}) 
 
--- static/myjss.js
+++ static/myjss.js
@@ -3,14 +3,30 @@ document.addEventListener("DOMContentLoaded", () => {
   newUser(user);
 });
 
-function loadPage() {
-  document.querySelector("#page").style.display = "block";
-}
-
 function newUser(user) {
   localStorage.setItem("user", user);
   const h3 = document.createElement("h3");
   h3.innerHTML = `Hi ${user}`;
   const user_div = document.querySelector("#show_username");
   user_div.append(h3);
+}
+
+function loadChannels() {
+  const request = new XMLHttpRequest();
+  request.open("POST", "/channels");
+  request.onload = () => {
+    const data = JSON.parse(request.responseText);
+    if (data.success) {
+      let numberOfChannels = data.channels.length;
+      for (let i = 0; i < numberOfChannels; i++) {
+        const li = document.createElement("li");
+        const channel_button = document.createElement("button");
+        channel_button.value = data.channels[i].name;
+        li.innerHTML = channel_button;
+        document.querySelector("#channels_list").append(li);
+      }
+    } else {
+      alert("error message");
+    }
+  };
 }
