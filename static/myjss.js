dac55bb737a655e9cff0e4a7d51ae4ebbfd7bafb
junzhong loo <junzhongloo@gmail.com>
2020-05-04 15:35:44 +0800

    display user DONE

--- static/myjss.js
+++ static/myjss.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#login-form").onsubmit = function () {
-    document.querySelector("#login-form-div").style.display = "none";
-    const user = document.querySelector("#username").value;
-    localStorage.setItem("user", user);
-    return false;
-  };
+  const user = prompt("Please type in your username");
+  newUser(user);
 });
 
 function loadPage() {
   document.querySelector("#page").style.display = "block";
+}
+
+function newUser(user) {
+  localStorage.setItem("user", user);
+  const h3 = document.createElement("h3");
+  h3.innerHTML = `Hi ${user}`;
+  const user_div = document.querySelector("#show_username");
+  user_div.append(h3);
 }
